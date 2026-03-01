c8a2423d3b9d921d63cc8af968bf9bfd7c820007
Leonardo Blanco <leonardo.blanco@polijunior.com.br>
2026-03-01 11:37:59 -0300

    finalizar site

---
+++ assets/js/login.js
@@ -0,0 +1,40 @@
+
+//sistema de login:
+
+if (!localStorage.getItem("usuario")) {
+  localStorage.setItem("usuario", JSON.stringify({
+    usuario: "Joaozinho",
+    senha: "Senha123"
+  }));
+} // salva um usuário de exemplo no localStorage
+
+
+if (localStorage.getItem("logado") === "true") {
+    window.location.href = "index.html"; // redireciona para a página principal se já estiver logado
+}
+
+const form = document.getElementById("inputsLogin");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // impede recarregar a página
+
+    const usuarioDigitado = document.getElementById("usuarioInput").value;
+    const senhaDigitada = document.getElementById("senhaInput").value;
+
+    const usuarioSalvo = JSON.parse(localStorage.getItem("usuario"));
+
+    if (
+        usuarioDigitado === usuarioSalvo.usuario &&
+        senhaDigitada === usuarioSalvo.senha
+    ) {
+        alert("Login realizado com sucesso!");
+        
+        // salvar sessão
+        localStorage.setItem("logado", "true");
+
+        // redirecionar
+        window.location.href = "index.html";
+    } else {
+        alert("Usuário ou senha incorretos!");
+    }
+});
